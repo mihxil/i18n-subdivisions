f0bb821b7a187baf5b9647fb8d060ce346dfd24c
Michiel Meeuwissen <git@meeuw.org>
2022-05-16 21:48:45 +0200

    Download new versions from wikipedia.

--- src/build/groovy/downloadSubdivisions.groovy
+++ src/build/groovy/downloadSubdivisions.groovy
@@ -5,10 +5,12 @@ buildResources.mkdirs()
 CountryCode.values().each {
     if (it.assignment == CountryCode.Assignment.OFFICIALLY_ASSIGNED) {
         String html = null
-        URL url = new URL("https://www.unece.org/fileadmin/DAM/cefact/locode/Subdivision/${it.alpha2.toLowerCase(Locale.US)}Sub.htm")
+        URL url = new URL("https://unece.org/fileadmin/DAM/cefact/locode/Subdivision/${it.alpha2.toLowerCase(Locale.US)}Sub.htm")
+
 
         try {
-            html = url.getText("UTF-8")
+            // seems to broken. All urls are giving 404 nowadays
+            html = url.getText(["user-agent":"User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36"], "UTF-8")
         } catch (IOException e) {
             println(e.getMessage())
         }
